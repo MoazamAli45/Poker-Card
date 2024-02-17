40112ed257b8e7e37f4d64b17963f4a34e8bf701
MoazamAli45 <21-se-48@students.uettaxila.edu.pk>
2024-02-17 16:57:57 +0500

    Refactor modal animation and update styles

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ function openModal(cardNumber) {
   var modal = document.getElementById("myModal");
   var cardNumberElement = document.getElementById("card-heading");
   cardNumberElement.textContent = cardNumber;
-  modal.style.transform = "translateY(-120%)"; // Move modal into view
+
+  const modalGroup = document.querySelector(".modal__group");
   //   show overlay
   var overlay = document.getElementById("overlay");
+  setTimeout(function () {
+    modal.style.opacity = "1";
+    modalGroup.style.transform = "translateY(-80%)";
+  }, 150);
+  modal.style.display = "flex";
+
   overlay.style.display = "block";
 }
 
 function closeModal() {
   var modal = document.getElementById("myModal");
-  modal.style.transform = "translateY(100%)"; // Move modal out of view
+  const modalGroup = document.querySelector(".modal__group");
   setTimeout(function () {
-    modal.style.display = "block";
+    modal.style.display = "none";
+
     overlay.style.display = "none";
-  }, 300); // After 0.3s (duration of transition), hide modal
+  }, 150); // After 0.3s (duration of transition), hide modal
+  modal.style.opacity = "0";
 }
